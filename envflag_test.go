c4c0f5fc4fb9dfa609b424ce72820ab640e49799
roylee0704 <roylee0704@gmail.com>
2016-08-30 16:55:19 +0800

    add testcase

--- envflag_test.go
+++ envflag_test.go
@@ -14,7 +14,7 @@ func setupFlag(options flagOption) *Envflag {
 	}
 }
 
-// set one flag via cli, expects empty unsetFlags
+// declare one flag, assigns value via cli, expects empty unsetFlags
 func TestOneSetFlag(t *testing.T) {
 	var testInt int
 
@@ -25,8 +25,28 @@ func TestOneSetFlag(t *testing.T) {
 	ef.parseWithEnv([]string{"--test-int=100"})
 
 	if want, got := 0, len(ef.unsetFlags()); want != got {
-		t.Errorf("non-empty unsetflags. (want)%v != %v(got)", want, got)
+		t.Errorf("expects only %v unset flag, got %v intead.", want, got)
 	}
 }
 
+// declare two flags, only set 1 via cli, expects 1 unsetflag
+func TestTwoFlagsOneUnset(t *testing.T) {
+	var (
+		testIntOne int
+		testIntTwo int
+	)
+
+	ef := setupFlag(func(fs *flag.FlagSet) {
+		fs.IntVar(&testIntOne, "test-int-one", 1, "this is test int one")
+		fs.IntVar(&testIntTwo, "test-int-two", 1, "this is test int two")
+	})
+
+	ef.parseWithEnv([]string{"--test-int-one=100"})
+
+	if want, got := 1, len(ef.unsetFlags()); want != got {
+		t.Errorf("expects only %v unset flag, got %v intead.", want, got)
+	}
+}
+
+// flagOption for testing purposes -- visibility
 type flagOption func(*flag.FlagSet)
